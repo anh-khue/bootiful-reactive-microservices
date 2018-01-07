bae8ce7802bb829384d9408fc31d1eee2c626d74
khuetla <thailyanhkhue@gmail.com>
2018-01-07 22:20:14 +0700

    Use centralized configuration with Config Service. Change to bean for data init.

--- reservation-service/src/main/java/com/khuetla/reactive/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/khuetla/reactive/reservationservice/ReservationServiceApplication.java
@@ -4,9 +4,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -37,6 +40,23 @@ public class ReservationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+}
+
+
+@RestController
+@RefreshScope
+class MessageRestController {
+
+    private final String message;
+
+    MessageRestController(@Value("${message}") String message) {
+        this.message = message;
+    }
+
+    @GetMapping("/message")
+    Publisher<String> getMessage() {
+        return Mono.just(this.message);
     }
 }
 
