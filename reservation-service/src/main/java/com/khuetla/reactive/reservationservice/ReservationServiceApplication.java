6ff88684f9df6b042cedfa2554ecc8d923d45991
khuetla <thailyanhkhue@gmail.com>
2018-01-07 17:28:24 +0700

    Use bean for data init. Test annotation-driven on WebFlux.

--- reservation-service/src/main/java/com/khuetla/reactive/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/khuetla/reactive/reservationservice/ReservationServiceApplication.java
@@ -4,18 +4,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
+
+    @Bean
+    ApplicationRunner dataInit(ReservationRepository reservationRepository) {
+        return args ->
+                reservationRepository
+                        .deleteAll()
+                        .thenMany(
+                                Flux.just("Java", "Servlet", "Spring",
+                                        "Microservices", "Cloud Native", "Serverless")
+                                        .map(Reservation::new)
+                                        .flatMap(reservationRepository::save))
+                        .thenMany(reservationRepository.findAll())
+                        .subscribe(System.out::println);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
@@ -23,26 +41,24 @@ public class ReservationServiceApplication {
 }
 
 
-@Component
-class DataInitializer implements ApplicationRunner {
+@RestController
+@RequestMapping("/reservations")
+class ReservationApiRestController {
 
     private final ReservationRepository reservationRepository;
 
-    DataInitializer(ReservationRepository reservationRepository) {
+    ReservationApiRestController(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        reservationRepository
-                .deleteAll()
-                .thenMany(
-                        Flux.just("Java", "Servlet", "Spring",
-                                "Microservices", "Cloud Native", "Serverless")
-                                .map(Reservation::new)
-                                .flatMap(reservationRepository::save))
-                .thenMany(reservationRepository.findAll())
-                .subscribe(System.out::println);
+    @GetMapping
+    Flux<Reservation> getReservations() {
+        return this.reservationRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    Mono<Reservation> getReservationIdBy(@PathVariable("id") String id) {
+        return this.reservationRepository.findById(id);
     }
 }
 
